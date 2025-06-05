8bab920e3bfb87dc7423e85dbf58d7184124445c
Zafarali Tolibov <tolibovzafarali7@gmail.com>
2025-06-05 10:58:34 -0400

    Completed the practice

--- src/main/java/org/example/CollectionChoiceExercises.java
+++ src/main/java/org/example/CollectionChoiceExercises.java
@@ -1,5 +1,10 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+
 // For these exercises, you need to declare a single variable
 // in each function that uses the appropriate collection type
 // (Array, ArrayList, HashSet, HashMap)
@@ -13,24 +18,47 @@ public class CollectionChoiceExercises {
         // of three strings that are known ahead of time to be "Red", "Green" and "Blue"
         // and will not change during program execution.
 
-        String[] colors;
+        String[] colors = new String[3];
+
+        colors[0] = "Red";
+        colors[1] = "Green";
+        colors[2] = "Blue";
+
+        System.out.println(Arrays.toString(colors));
     }
 
     public void MakeStudentCollection() {
         // Create an appropriate collection variable for holding a list of student names
         // that will be added to and removed from during program execution.
+
+        ArrayList<String> studentNames = new ArrayList<>();
+
+        studentNames.add("Darren");
+        studentNames.add("Savion");
+        studentNames.add("Ali");
+        System.out.println(studentNames);
     }
 
     public void MakeLoggedInUsersCollection() {
         // Create an appropriate collection variable for holding a set of
-        // unique logged in usernames where adding a username that's already
+        // unique logged-in usernames where adding a username that's already
         // present is ignored and checking if a user is in the collection can
         // be done very quickly.
+
+        HashSet<String> usernames = new HashSet<>();
+        usernames.add("savionRj");
+
+        System.out.println(usernames);
     }
 
     public void MakeEmployeeAccessCodesCollection() {
         // Create an appropriate collection variable for holding a set of
         // key-value pairs where they key is an employee name (String) and the
         // value is an employee access code (String.)
+
+        HashMap<String, String> employeeAccess = new HashMap<>();
+        employeeAccess.put("SavionRj", "aunfna93q" );
+
+        System.out.println(employeeAccess);
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,5 +2,11 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
+        CollectionChoiceExercises functions = new CollectionChoiceExercises();
+
+        functions.MakeColorCollection();
+        functions.MakeStudentCollection();
+        functions.MakeLoggedInUsersCollection();
+        functions.MakeEmployeeAccessCodesCollection();
     }
 }
